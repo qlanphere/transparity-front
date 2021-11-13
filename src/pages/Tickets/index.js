bae99c53360bd0f21b48f2f2d31e3df2d2b5128e
Quinn Lanphere <qlanphere@yahoo.com>
2021-11-13 16:32:49 +0000

    added tickets page with form for adding ticket

--- src/pages/Tickets/index.js
+++ src/pages/Tickets/index.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 import { useAuthContext } from '../../contexts/auth'
 
 const host = 'https://transparity.herokuapp.com'
@@ -8,14 +9,30 @@ const Tickets = () => {
     const {currentUser} = useAuthContext()
     const [openTickets, setOpenTickets] = useState([])
     const [closedTickets, setClosedTickets] = useState([])
+    const [ticketFormData, setTicketFormData] = useState({name: "", description: "", status: true})
 
-    useEffect = (() => {
+    const handleInput = e => setTicketFormData(prev => ({ ...prev, [e.target.name]: e.target.value }))
+    const formIncomplete = () => Object.values(ticketFormData).some(v => !v)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await axios.patch(`${host}/ticket/${currentUser.sub.id}`)
+
+    }
+
+    useEffect(() => {
 
         const getTickets = async () => {
 
-            let response = await fetch(`${host}/ticket/${currentUser.sub.id}`)
+            let response = await fetch(`${host}/user/${currentUser.sub.id}`)
             let ticketData = await response.json()
-            console.log(ticketData)
+            let ticketArray = ticketData.tickets
+            if (ticketArray) {
+                let openTicks = ticketArray.filter(ticket => ticket.status == true)
+                let closedTicks = ticketArray.filter(ticket => ticket.status == false)
+                setOpenTickets(openTicks)
+                setClosedTickets(closedTicks)
+            }
 
         }
         getTickets()
@@ -23,13 +40,22 @@ const Tickets = () => {
 
     return (
         <>
+        <div>
+            <h1>Create New Ticket</h1>
+            <form>
+                <input type = "text" name="name" value={ticketFormData.name} onChange={handleInput} placeholder="Title" />
+                <input type = "text" name="description" value={ticketFormData.description} onChange={handleInput} placeholder="description" />
+                <input type="submit" onSubmit={handleSubmit} className={formIncomplete() ? 'disabled' : 'enabled'} disabled={formIncomplete()}/>
+            </form>
+
+        </div>
             <div>
                 <h1>Open Tickets</h1>
-
+                    {openTickets}
             </div>
             <div>
                 <h1>Closed Tickets</h1>
-
+                    {closedTickets}
             </div>
         </>
     )
