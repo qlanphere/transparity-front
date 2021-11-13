5bc0eaf3aff745f5990dcfc39295c81a2a8a69f4
Quinn Lanphere <qlanphere@yahoo.com>
2021-11-13 10:09:54 +0000

    added logout button, login, and register buttons that appear if you're logged in or out correctly

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import { ProSidebar, Menu, MenuItem, SubMenu, SidebarContent, SidebarHeader } from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
 import 'font-awesome/css/font-awesome.min.css';
-import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart } from 'react-icons/fa';
+import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart, FaSignOutAlt, FaSignInAlt, FaUserCircle } from 'react-icons/fa';
 import './Sidebar.css'
 import {NavLink, Link } from 'react-router-dom';
 import { Nav, Navbar, Container } from 'react-bootstrap'
+import { useAuthContext } from "../../contexts/auth";
 
 const Sidebar = () => {
+
+  const {currentUser, logout} = useAuthContext()
+
   return (
     <div className="bar">
       <ProSidebar>
@@ -29,6 +33,9 @@ const Sidebar = () => {
           <Menu iconShape="circle">
           <MenuItem icon={<FaTachometerAlt />}><Nav.Link href = "/home">Home</Nav.Link></MenuItem>
           <MenuItem icon={<FaGem />}>About</MenuItem>
+          {currentUser && <MenuItem icon = {<FaSignOutAlt />}><Nav.Link href = "/home" onClick = {logout}>Logout</Nav.Link></MenuItem>}
+          {!currentUser && <MenuItem icon = {<FaSignInAlt />}><Nav.Link href = "/login">Sign in</Nav.Link></MenuItem>}
+          {!currentUser && <MenuItem icon = {<FaUserCircle />}><Nav.Link href = "/register">Register</Nav.Link></MenuItem>}
           <SubMenu icon={<FaHeart />} title="Charities" >
             <MenuItem>Upcoming Events</MenuItem>
             <MenuItem> <Nav.Link href="/feedbackForm">Feedback Page</Nav.Link></MenuItem>
