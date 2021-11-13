33919d0487b435b43af4f9baa9ced9424ff99c0e
velinkalenderski <velinkalenderski@hotmail.co.uk>
2021-11-13 18:29:01 +0000

    some styling added to card

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,14 +3,14 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+import './Post.css'
 
 import { useCharityContext } from '../../contexts/charityContext';
 import { useHistory } from 'react-router-dom';
 
 const Post = (props) => {
 
-    const {setCharityName} = useCharityContext()
+    const { setCharityName } = useCharityContext()
     const history = useHistory();
 
     const handleClick = (name) => {
@@ -21,26 +21,18 @@ const Post = (props) => {
 
     return (
         <>
-            <Card sx={{ maxWidth: 2000 }} className = "mb-3">
-                <div className="d-flex align-items">
-                    <CardMedia
-                        style={{ height: "400px", width: "400px" }}
-                        className="col"
-                        component="img"
-                        image={props.image}
-                    />
-                    <CardContent className="col">
-                        <h1>{props.title}</h1>
-                        <p>{props.description}</p>
-                        <p>{props.date}</p>
-                        <CardActions className = "justify-content-center">
-                            <Button size="small" className = "mt-auto">Donate</Button>
-                            <Button size="small" onClick = {()=> handleClick(props.name)}>Learn More</Button>
-                        </CardActions>
-                    </CardContent>
+            <div className="card-container">
+                <img className="card-img" src={props.image} width={150} />
+                <div className="card-info">
+                    <h1 className="card-title">{props.title}</h1>
+                    <p className="card-description">{props.description}</p>
+                    <p>{props.goal}</p>
+                    <p className="card-date">{props.date}</p>
 
+                    <button className="t-button" size="small">Donate</button>
+                    <button className="t-button" size="small" onClick={() => handleClick(props.name)}>Learn More</button>
                 </div>
-            </Card>
+            </div>
         </>
     );
 }
