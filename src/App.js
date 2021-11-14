27632824f6c9d50ce8206bdefaccc91f50600a60
velinkalenderski <velinkalenderski@hotmail.co.uk>
2021-11-14 08:12:34 +0000

    resolve git conflict + CharityPost import missing on App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-
 import './App.css';
 import Sidebar from './components/Sidebar';
-<<<<<<< HEAD
-import { Login, Register, Home, Feedback, CharityPage, DonationsPage, CharityPost} from './pages'
-=======
-import { Login, Register, Home, Feedback, CharityPage, DonationsPage, Tickets, TicketId} from './pages'
->>>>>>> 42bfef72a1e2c6ed9aab49d06cb990f3cf44c807
+import { Login, Register, Home, Feedback, CharityPage, CharityPost, DonationsPage, Tickets, TicketId } from './pages'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as CustomRoutes from "./routing";
 import {
@@ -17,38 +12,38 @@ import {
 function App() {
   return (
     <div className="App">
-        <Sidebar/>
+      <Sidebar />
       <Router>
         <Switch>
-          <Route exact path = "/home">
-              <Home />
+          <Route exact path="/home">
+            <Home />
           </Route>
-          <Route exact path = "/">
-              <Home />
+          <Route exact path="/">
+            <Home />
           </Route>
           <Route exact path="/login">
             <Login />
           </Route>
-          <Route exact path = "/register">
+          <Route exact path="/register">
             <Register />
           </Route>
-          <CustomRoutes.LoggedOutRoute exact path = "/feedbackForm">
+          <CustomRoutes.LoggedOutRoute exact path="/feedbackForm">
             <Feedback />
-          </CustomRoutes.LoggedOutRoute> 
-          <Route exact path = "/charities/:name">
+          </CustomRoutes.LoggedOutRoute>
+          <Route exact path="/charities/:name">
             <CharityPage />
           </Route>
-          <CustomRoutes.LoggedOutRoute exact path = "/donations">
+          <CustomRoutes.LoggedOutRoute exact path="/donations">
             <DonationsPage />
           </CustomRoutes.LoggedOutRoute>
-          <CustomRoutes.LoggedOutRoute exact path = "/charitypost">
+          <CustomRoutes.LoggedOutRoute exact path="/charitypost">
             <CharityPost />
           </CustomRoutes.LoggedOutRoute>
-          <CustomRoutes.LoggedOutRoute exact path = "/tickets">
+          <CustomRoutes.LoggedOutRoute exact path="/tickets">
             <Tickets />
           </CustomRoutes.LoggedOutRoute>
-          <CustomRoutes.LoggedOutRoute exact path = "/tickets/:id">
-              <TicketId />
+          <CustomRoutes.LoggedOutRoute exact path="/tickets/:id">
+            <TicketId />
           </CustomRoutes.LoggedOutRoute>
         </Switch>
       </Router>
