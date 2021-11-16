e620d7d02458b337356e3b57969fbbd8cee7bf29
velinkalenderski <velinkalenderski@hotmail.co.uk>
2021-11-16 10:59:26 +0000

    donations history page wfunctionality added

--- src/pages/DonationsPage/index.js
+++ src/pages/DonationsPage/index.js
@@ -1,23 +1,47 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { useAuthContext } from '../../contexts/auth'
+import Transaction from '../../components/Transaction'
+import './Donations.css'
 
-const host = 'https://transparity.netlify.app'
+// const host = 'https://transparity.netlify.app'
+const host = 'http://localhost:5000'
 
 const DonationsPage = () => {
 
-    const {currentUser} = useAuthContext()
+    const { currentUser } = useAuthContext()
     const [donations, setDonations] = useState(0)
+    const [transactions, setTransactions] = useState([])
 
-    let dummyData = [{amount: 12}, {amount: 25}, {amount: 72}]
+    let dummyData = [{ amount: 12 }, { amount: 25 }, { amount: 72 }]
 
-    useEffect (() => {
-        
+    useEffect(() => {
+
         const getDonations = async () => {
-            // let response = await fetch(`${host}/${currentUser.id}`)
-            // let userData = await response.json()
-            // let donationData = userData.donations
-            let donationArray = dummyData.map(element => element.amount)
-            setDonations(donationArray.reduce((a, b) => a + b, 0)) 
+            const options = {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Access-Control-Allow-Origin": "*",
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`
+                },
+                mode: 'cors',
+            }
+            let response = await fetch(`${host}/user/${currentUser.sub.id}`, options)
+            // let data = await response.json()
+            // console.log(data)
+            // console.log(data.donations)
+            // console.log(data.donations[0]['amount'])
+            let userData = await response.json()
+            let donationData = userData.donations
+
+            let transactionArray = donationData.map(transaction =>
+                <Transaction amount={transaction.amount}
+                    charity_name={transaction.charity_name}
+                    date={transaction.date} />)
+
+            let donationArray = donationData.map(element => element.amount)
+            setDonations(donationArray.reduce((a, b) => a + b, 0))
+            setTransactions(transactionArray)
         }
 
         getDonations()
@@ -25,10 +49,11 @@ const DonationsPage = () => {
     }, [])
 
     return (
-        <>
-            <h1>{currentUser.user=='charity' ? `Donations Received`:`${currentUser.sub.name}'s Donations`}</h1>
-            <h3>${donations}</h3>
-        </>
+        <div className="donations-history-page">
+            <h1 className="donations-history-title">{currentUser.user == 'charity' ? `Donations Received` : `${currentUser.sub.name}'s Total Amount Donated: `}</h1>
+            <h3 className="total-donated"><span className="green">$</span>{donations}</h3>
+            <div>{transactions}</div>
+        </div>
     )
 }
 
